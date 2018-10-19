7d4e816641d1fa842b71268c3b88b3708f7510a2
unknown <elmfer10@gmail.com>
2018-10-18 20:06:38 -0500

    Setuped & added LINEAR function

--- src/com/elmfer/alasen/util/Easing.java
+++ src/com/elmfer/alasen/util/Easing.java
@@ -1,5 +1,33 @@
 package com.elmfer.alasen.util;
 
-public enum Easing {
+import java.util.function.Function;
 
+public enum Easing {
+	
+	LINEAR(new Function<Double, Double>(){
+		
+		@Override
+		public Double apply(Double t) {
+			
+			return t;
+		}
+	});
+	
+	private final Function<Double, Double> function;
+	
+	Easing(Function<Double, Double> timeShader){
+		
+		function = timeShader;
+	}
+	
+	public Function<Double, Double> getFunction(){
+		
+		return function;
+	}
+	
+	public static Easing getDefault(){
+		
+		return LINEAR;
+	}
+	
 }
